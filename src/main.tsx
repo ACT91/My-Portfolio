cc83858f898218ec7763666ff7c7a20564b8bee1
Stanley Gersom <mwactac@gmail.com>
2025-06-18 14:41:24 +0200

    I've added Vercel Speed Insights to my application by:
    Importing the SpeedInsights component from @vercel/speed-insights/react
    
    Adding the <SpeedInsights /> component inside my application's component tree

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import ThemeProvider from './ThemeProvider'
-import { injectSpeedInsights } from '@vercel/speed-insights';
-injectSpeedInsights();
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>,
 )
